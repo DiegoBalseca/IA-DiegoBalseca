bb2fd324cf8225bde3428f31b3a81a2c8b09cdaf
aa <a@a>
2025-12-02 13:25:53 +0100

    2-12-25

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -13,15 +13,195 @@ public class enemy : MonoBehaviour
     }
 
     public EnemyState currentState;
-    
+
+    Transform _player;
+    Vector3 _playerLastPositionKnown;
+
+    //cosas patrullar
+
+    [SerializeField] private Transform[] _patrolPoints;
+
+    //cosas detección
+    [SerializeField] private float _detectionRange = 7;
+    [SerializeField] private float _detectionAngle = 90;
+
+    //cosas busqueda
+
+    private float _searchTimer;
+
+    [SerializeField] private float _searchWaitTime = 15;
+
+    [SerializeField] private float _searchRadius = 10;
+
+
+    void Awake()
+    {
+        _enemyAgent = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindWithTag("Player").transform;
+    }
+
     void Start()
     {
-        
+        currentState = EnemyState.Patrolling;
+        SetRandomPatrolPoint();
     }
 
     
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case EnemyState.Patrolling:
+                Patrol();
+
+            break;
+            case EnemyState.Chasing:
+                Chase();
+
+            break;
+            case EnemyState.Searching:
+                Search();
+
+            break;
+            default:
+                Patrol();
+
+            break;
+        }
     }
+
+    void Patrol()
+    {
+        if(OnRange())
+        {
+            currentState = EnemyState.Chasing;
+        }
+        if(_enemyAgent.remainingDistance < 0.5f)
+        {
+            SetRandomPatrolPoint();
+        }
+
+
+
+
+
+       if(_enemyAgent.remainingDistance < 0.5f)
+        {
+            SetRandomPatrolPoint();
+        } 
+        SetRandomPatrolPoint();
+    }
+
+    void Chase()
+    {
+        if(!OnRange())
+        {
+            currentState = EnemyState.Searching;
+        }
+        _enemyAgent.SetDestination(_player.position);
+
+        _playerLastPositionKnown = _player.position;
+    }
+
+    void Search()
+    {
+        if(OnRange())
+        {
+            currentState = EnemyState.Chasing;
+        }
+
+        _searchTimer += Time.deltaTime;
+
+        if(_searchTimer < _searchWaitTime)
+        {
+            if(_enemyAgent.remainingDistance < 0.5f)
+            {
+                Vector3 randomPoint;
+                if(RandomSearchPoint(_playerLastPositionKnown, _searchRadius, out randomPoint))
+                {
+                    _enemyAgent.SetDestination(randomPoint);
+                }
+            }
+        }
+        else
+        {
+            currentState = EnemyState.Patrolling;
+            _searchTimer = 0;
+        }
+    }
+
+    bool RandomSearchPoint(Vector3 center, float radius, out Vector3 point)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * radius;
+
+        NavMeshHit hit;
+        if(NavMesh.SamplePosition(randomPoint, out hit, 4, NavMesh.AllAreas))
+        {
+            point = hit.position;
+            return true;
+        }
+
+        point = Vector3.zero;
+        return false;
+
+    }
+
+    void SetRandomPatrolPoint()
+    {
+        _enemyAgent.SetDestination(_patrolPoints[Random.Range(0, _patrolPoints.Length)].position);
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        foreach (Transform point in _patrolPoints)
+        {
+            Gizmos.DrawWireSphere(point.position, 0.3f);
+        }
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _detectionRange);
+
+        Gizmos.color = Color.yellow;
+
+        Vector3 fovLine1 = Quaternion.AngleAxis(_detectionAngle * 0.5f, transform.up) * transform.forward * _detectionRange;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-_detectionAngle * 0.5f, transform.up) * transform.forward * _detectionRange;
+
+        Gizmos.DrawLine(transform.position, transform.position + fovLine1);
+        Gizmos.DrawLine(transform.position, transform.position + fovLine2);
+    }
+
+    bool OnRange()
+    {
+        /*if(Vector3.Distance(transform.position, _player.position) < _detectionRange)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }*/
+
+    Vector3 directionToPlayer = _player.position - transform.position;
+    float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+    float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
+
+    if(distanceToPlayer > _detectionRange)
+        {
+            return false;
+        }
+
+        if (angleToPlayer > _detectionAngle * 0.5f)
+        {
+            return false;
+        }
+
+        return true;
+
+    
+    }
+
+
 }
